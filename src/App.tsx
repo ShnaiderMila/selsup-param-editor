0bbe63404adedc29a5ce581891335f5086237f1c
Mila Shnaider <shnaiderln@mail.ru>
2025-02-05 15:38:19 +0300

    feat: Editor done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ExampleUsage from './ParamEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Редактор параметров</h1>
+      <ExampleUsage />
+    </div>
+  );
+}
+
+export default App;
